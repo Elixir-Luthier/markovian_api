053e9236a9fd098fb6b68b647ac58e6528409689
Nisarga Patel <np8@umbc.edu>
2017-04-05 15:06:03 -0400

    fixed deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MarkovianApi.Mixfile do
     [
       app: :markovian_api,
       version: "0.1.0",
-      elixir: "~> 1.5-dev",
+      elixir: ">= 1.4.0",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
